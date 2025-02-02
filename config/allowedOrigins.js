81b29cac2cbf4d34eae3dd27a2aa1cd165337baf
Dennis Paixão <dennispaixaoads@gmail.com>
2025-02-02 15:05:16 -0300

    teste cors 2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,2 +1,3 @@
-const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",").map(origin => origin.trim();
+console.log(allowedOrigins);
 module.exports = allowedOrigins;
